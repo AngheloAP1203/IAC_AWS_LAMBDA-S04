59368a3ee2ec5b150d0c7576faca86fc83bbd05e
AngheloAP1203 <angheloalcantaraperez@gmail.com>
2026-05-06 04:10:09 -0500

    feat: configurar roles y politicas iam con minimo privilegio para las funciones lambda

---
+++ iac/iam.tf
@@ -0,0 +1,95 @@
+# --- POLÍTICA DE ASUNCIÓN DE ROL (compartida) ---
+data "aws_iam_policy_document" "lambda_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
+# --- ROL: upload-lambda-role ---
+resource "aws_iam_role" "upload_lambda_role" {
+  name               = "upload-lambda-role-${var.environment}"
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "upload_basic_execution" {
+  role       = aws_iam_role.upload_lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_role_policy_attachment" "upload_vpc_execution" {
+  role       = aws_iam_role.upload_lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+}
+
+resource "aws_iam_policy" "upload_policy" {
+  name = "upload-lambda-policy-${var.environment}"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect   = "Allow"
+      Action   = ["s3:PutObject"]
+      Resource = ["${aws_s3_bucket.images.arn}/uploads/*"]
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "upload_s3_attach" {
+  role       = aws_iam_role.upload_lambda_role.name
+  policy_arn = aws_iam_policy.upload_policy.arn
+}
+
+# --- ROL: crop-lambda-role ---
+resource "aws_iam_role" "crop_lambda_role" {
+  name               = "crop-lambda-role-${var.environment}"
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "crop_basic_execution" {
+  role       = aws_iam_role.crop_lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_role_policy_attachment" "crop_vpc_execution" {
+  role       = aws_iam_role.crop_lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+}
+
+resource "aws_iam_policy" "crop_policy" {
+  name = "crop-lambda-policy-${var.environment}"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = ["s3:GetObject"]
+        Resource = ["${aws_s3_bucket.images.arn}/uploads/*"]
+      },
+      {
+        Effect   = "Allow"
+        Action   = ["s3:PutObject"]
+        Resource = ["${aws_s3_bucket.images.arn}/processed/*"]
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "sqs:ReceiveMessage",
+          "sqs:DeleteMessage",
+          "sqs:GetQueueAttributes",
+          "sqs:ChangeMessageVisibility"
+        ]
+        Resource = [aws_sqs_queue.image_queue.arn]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "crop_s3_sqs_attach" {
+  role       = aws_iam_role.crop_lambda_role.name
+  policy_arn = aws_iam_policy.crop_policy.arn
+}
